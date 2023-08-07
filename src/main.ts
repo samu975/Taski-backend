dcd04a95a889b9a9db6f62552606660a63d35d55
Samuel Rosero <saferoxx215@gmail.com>
2023-08-07 17:47:11 -0500

    added the port variable

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,6 @@ async function bootstrap() {
     optionSuccessStatus: 200,
   };
   app.use(cors(corsOption));
-  await app.listen(process.env.BACKEND_PORT || 3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
